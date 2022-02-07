f469b15c17223ffc0bce7377d231d980ca9db47c
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-02-07 15:55:30 +0100

    Add files via upload

--- api/app.js
+++ api/app.js
@@ -3,12 +3,17 @@ const express = require("express"),
     cors = require("cors"),
     app = express();
 
+const { Client, Intents } = require('discord.js');
+
 app.use(cors());
 
-
+/// OAuth2 
+const fetch = require("node-fetch")
+const randomstring = require("randomstring");
 
 require("dotenv").config()
 const PORT = process.env.PORT || '80';
+const CLIENT_SECRET = process.env.port || 'WmrM3betfCuAIfAP2uFkivIhDn8soUfk';
 
 // api things 
 
@@ -19,7 +24,8 @@ var corsOptions = {
 }
 
 const Discord = require("discord.js"),
-    client = new Discord.Client();
+    client = new Discord.Client({ intents: [Intents.FLAGS.GUILD_MEMBERS] })
+/// Intent: { intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD] }
 
 const name = require("../package.json").name,
     description = require("../package.json").description,
@@ -33,15 +39,38 @@ app.get("/", cors(corsOptions), (req, res) => {
     return res.send(mainpage)
 });
 
+app.get("/callback", cors(corsOptions), (req, res) => {
+    const fragment = new URLSearchParams();
+    const [accessToken, tokenType] = [fragment.get('code'), fragment.get('token_type')];
+
+    if (!accessToken) {
+        const callback = (`Hello! Here is your api token: ${fragment.get('code')}`);
+        return res.send(callback)
+    }
+    fetch('https://discord.com/api/users/@me', {
+        headers: {
+            authorization: `${tokenType} ${accessToken}`,
+        },
+    })
+        .then(result => result.json())
+        .then(response => {
+            return
+        })
+        .catch(console.error);
+    console.log(accessToken)
+    const callback = (`Hello! Here is your api token: ${fragment.get('code')}`);
+    return res.send(callback)
+});
+
 app.get("/v1", cors(corsOptions), (req, res) => {
-    const v1page = (`This is the v1 page from ${name}`);
+    const v1page = (`Oops! The v1 route is only for the v1 version! https://invalidlenni.gitbook.io/disweb/ for Documentation`);
     return res.send(v1page) 
 });
 
 // GET USER INFORMATION
 app.get("/v1/user/:userID", cors(corsOptions), (req, res) => {
     client.users.fetch(req.params.userID).then((user) => {
-        const results = ({ username: `${user.username}`, bot: user.bot, discriminator: `${user.discriminator}`, avatar_url: `${user.displayAvatarURL({ format: "png", size: 4096, dynamic: true })}`, creation_date: `${user.createdAt}`, creation_timestamp: user.createdTimestamp});
+        const results = ({ username: `${user.username}`, is_bot: user.bot, discriminator: `${user.discriminator}`, avatar_url: `${user.displayAvatarURL({ format: "png", size: 4096, dynamic: true })}`, banner_url: `${user.bannerURL({ format: "png", dynamic: true })}`, creation_date: `${user.createdAt}`, creation_timestamp: user.createdTimestamp});
         return res.send(results);
     });
 });
@@ -49,26 +78,33 @@ app.get("/v1/user/:userID", cors(corsOptions), (req, res) => {
 // THE BOT MUST BE ON THE GUILD FOR FETCH THE GUILD INFORMATIONS!
 app.get("/v1/guild/:guildID", cors(corsOptions), (req, res) => {
     client.guilds.fetch(req.params.guildID).then((guild) => {
-        const results = ({ guildID: `${guild.id}`, guildname: `${guild.name}`, guildicon_url: `${guild.iconURL({ size: 4096, dynamic: true })}`, guildroles_count: `${guild.roles.cache.size}`, guildusers_count: `${guild.members.cache.size}`, guildemojis_count: `${guild.emojis.cache.size}`, guildownerID: `${guild.ownerID}`, guildcreation_date: `${guild.createdAt}`, guildcreation_timestamp: guild.createdTimestamp });
+        const results = ({ guildID: `${guild.id}`, guildname: `${guild.name}`, guildicon_url: `${guild.iconURL({ size: 4096, dynamic: true })}`, guildroles_count: `${guild.roles.cache.size}`, guildusers_count: `${guild.members.cache.size}`, guildemojis_count: `${guild.emojis.cache.size}`, guildownerID: `${guild.ownerID}`, guildcreation_date: `${guild.createdAt}`, guildcreation_timestamp: guild.createdTimestamp});
+        return res.send(results);
+    });
+});
+
+app.get("/v1/invites/:INVITEURL", cors(corsOptions), (req, res) => {
+    client.invite.fetch(req.params.INVITEURL).then((invite) => {
+        const results = ({ guildID: `${invite.guild.id}`, guildname: `${invite.guild.name}`, guildicon_url: `${invite.guild.iconURL({ size: 4096, dynamic: true })}`, guildroles_count: `${invite.guild.roles.cache.size}`, guildusers_count: `${invite.guild.members.cache.size}`, guildemojis_count: `${invite.guild.emojis.cache.size}`, guildownerID: `${invite.guild.ownerID}`, guildcreation_date: `${invite.guild.createdAt}`, guildcreation_timestamp: invite.guild.createdTimestamp});
         return res.send(results);
     });
 });
 
 app.use(function (req, res, next) {
-    res.status(404).send("Sorry, can't find that! The route is [/vX/user/:USERID] or [/vX/guild/:GUILDID]")
+    res.status(404).json({ message: '404: Not found', code: 404})
 });
 
 app.use(function (req, res, next) {
-    res.status(201).send("Missing parm")
+    res.status(201).send({ message: '201: Missing parameters', code: 201})
 });
 
 
 app.use(function (req, res, next) {
-    res.status(403).send("Missing access")
+    res.status(403).send({ message: '403: Missing access', code: 403})
 });
 
 app.use(function (req, res, next) {
-    res.status(500).send("Internal Server Error")
+    res.status(500).send({ message: '500: Internal Server Error', code: 500})
 });
 
 // API START
