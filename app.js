6c852374b70a51ed2f5a557160efe626fcd0a49c
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-02-08 16:34:40 +0100

    Add all ratelimits

--- app.js
+++ app.js
@@ -33,12 +33,38 @@ const name = require("../package.json").name,
     author = require("../package.json").author,
     url = require("../package.json").repository.url;
 
+    /* const apiLimiter = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    })*/
+
+// Ratelimits
+const userLimiter = rateLimit({
+	windowMs: 60 * 60 * 5, // 5 minutes
+	max: 5, // Limit each IP to 5 user requests per `window` (here, per minute)
+	message:
+		'You are being ratelimited! Try again later.',
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+const guildLimiter = rateLimit({
+	windowMs: 60 * 60 * 5, // 5 minutes
+	max: 5, // Limit each IP to 5 user requests per `window` (here, per minute)
+	message:
+		'You are being ratelimited! Try again later.',
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+
 // ROUTES
 app.get("/", cors(corsOptions), (req, res) => {
-    const mainpage = ({"API": `${name}`, "\description": `${description}`, "API Version is": `${version}`, "The Github Rep": `${url}` });
+    const mainpage = ({"API": `${name}`, "\description": `${description}`, "Made by": `${author}`, "API Version is": `${version}`, "The Github Rep": `${url}` });
     return res.send(mainpage)
 });
-
 
 app.get("/callback", cors(corsOptions), (req, res) => {
     const fragment = new URLSearchParams();
@@ -63,14 +89,13 @@ app.get("/callback", cors(corsOptions), (req, res) => {
     return res.send(callback)
 });
 
-
 app.get("/v1", cors(corsOptions), (req, res) => {
-    const v1page = (`Oops! The v1 route is only for the v1.X.X version! https://invalidlenni.gitbook.io/disweb/ for Documentation`);
+    const v1page = (`Oops! The v1 route is only for the v1 version! https://invalidlenni.gitbook.io/disweb/ for Documentation`);
     return res.send(v1page) 
 });
 
 // GET USER INFORMATION
-app.get("/v1/user/:userID", cors(corsOptions), (req, res) => {
+app.get("/v1/user/:userID", userLimiter, cors(corsOptions), (req, res) => {
     client.users.fetch(req.params.userID).then((user) => {
         const results = ({ username: `${user.username}`, is_bot: user.bot, discriminator: `${user.discriminator}`, avatar_url: `${user.displayAvatarURL({ format: "png", size: 4096, dynamic: true })}`, banner_url: `${user.bannerURL({ format: "png", dynamic: true })}`, creation_date: `${user.createdAt}`, creation_timestamp: user.createdTimestamp});
         return res.send(results);
@@ -78,38 +103,45 @@ app.get("/v1/user/:userID", cors(corsOptions), (req, res) => {
 });
 
 // THE BOT MUST BE ON THE GUILD FOR FETCH THE GUILD INFORMATIONS!
-app.get("/v1/guild/:guildID", cors(corsOptions), (req, res) => {
+app.get("/v1/guild/:guildID", guildLimiter, cors(corsOptions), (req, res) => {
     client.guilds.fetch(req.params.guildID).then((guild) => {
         const results = ({ guildID: `${guild.id}`, guildname: `${guild.name}`, guildicon_url: `${guild.iconURL({ size: 4096, dynamic: true })}`, guildroles_count: `${guild.roles.cache.size}`, guildusers_count: `${guild.members.cache.size}`, guildemojis_count: `${guild.emojis.cache.size}`, guildownerID: `${guild.ownerID}`, guildcreation_date: `${guild.createdAt}`, guildcreation_timestamp: guild.createdTimestamp});
         return res.send(results);
     });
 });
 
-app.get("/v1/widget/:GUILDID", cors(corsOptions), (req, res) => {
-    client.fetchGuildWidget(req.params.GUILDID).then((widget) => {
-        const results = ({ guildID: `${widget.guild.id}`, guildname: `${widget.guild.name}`, guildicon_url: `${widget.guild.iconURL({ size: 4096, dynamic: true })}`, guildroles_count: `${widget.guild.roles.cache.size}`, guildusers_count: `${widget.guild.members.cache.size}`, guildemojis_count: `${widget.guild.emojis.cache.size}`, guildownerID: `${widget.guild.ownerID}`, guildcreation_date: `${widget.guild.createdAt}`, guildcreation_timestamp: widget.guild.createdTimestamp});
+app.get("/v1/invites/:INVITEURL", guildLimiter, cors(corsOptions), (req, res) => {
+    client.invite.fetch(req.params.INVITEURL).then((invite) => {
+        const results = ({ guildID: `${invite.guild.id}`, guildname: `${invite.guild.name}`, guildicon_url: `${invite.guild.iconURL({ size: 4096, dynamic: true })}`, guildroles_count: `${invite.guild.roles.cache.size}`, guildusers_count: `${invite.guild.members.cache.size}`, guildemojis_count: `${invite.guild.emojis.cache.size}`, guildownerID: `${invite.guild.ownerID}`, guildcreation_date: `${invite.guild.createdAt}`, guildcreation_timestamp: invite.guild.createdTimestamp});
         return res.send(results);
     });
 });
 
+
+// Error
 app.use(function (req, res, next) {
-    res.status(404).send("Sorry, can't find that!")
+    res.status(404).json({ message: '404: Not found', code: 404})
 });
 
 app.use(function (req, res, next) {
-    res.status(201).send("Missing parameters!")
+    res.status(201).send({ message: '201: Missing parameters', code: 201})
 });
 
 
 app.use(function (req, res, next) {
-    res.status(403).send("Missing access")
+    res.status(403).send({ message: '403: Missing access', code: 403})
 });
+
+app.use(function (req, res, next) {
+    res.status(500).send({ message: '500: Internal Server Error', code: 500})
+});
+
 
 // API START
 client.on("ready", () => {
     console.log(`The API is now online! Bot: ${client.user.username}`)
 });
 
-app.listen(PORT, console.log(`discord-web-api is listing to`, PORT));
+app.listen(PORT, console.log(`disweb is listing to`, PORT));
 
 client.login(process.env.TOKEN);
